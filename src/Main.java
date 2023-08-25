59664400f2fa96b0dd63d8b6ddf6d3ccdb26b9cd
baderaldossary <baldossary@gmail.com>
2023-08-25 09:20:17 -0700

    COMP B11: Programming Projects

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
 
-            new NumberSquared();
             NumberSquared.squareNumbers(scanner);
         }
+
     }
 }
