fb03838603de65c353531473da7d7b1b231b0de3
baderaldossary <baldossary@gmail.com>
2023-08-25 10:32:08 -0700

    Project #2 Baseball

---
+++ src/Assignments/Baseball.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Baseball {
+    public static void base(Scanner scanner){
+        int dodgers;
+        int yankees;
+        int orioles;
+
+        System.out.print("Enter Dodgers number: ");
+        dodgers = scanner.nextInt();
+        System.out.print("Enter Yankees number: ");
+        yankees = scanner.nextInt();
+        System.out.print("Enter Orioles number: ");
+        orioles = scanner.nextInt();
+
+        System.out.println("Last night the Dodgers scored "+ dodgers +", ");
+        System.out.println("the Yankees scored "+ yankees +", and");
+        System.out.println("the Cubs scored "+ orioles +".");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-
-            // NumberSquared.squareNumbers(scanner); // #1 Square a Number
-            DoubleNumber.doubleNum(scanner); // #2 Double a Number
-
+            // NumberSquared.squareNumbers(scanner); // Project #1 Square a Number
+            // DoubleNumber.doubleNum(scanner); // Project #2 Double a Number
+            Baseball.base(scanner); // Project #3 Baseball
         }catch (Exception e){
             System.out.println("Something went wrong.! ");
         }
