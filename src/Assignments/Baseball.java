0d2d314cc5f3c28cbf221ddf62b50bff6789e8a5
Bader Aldossary <56875000+bsd474@users.noreply.github.com>
2023-08-29 19:07:39 -0700

    Update Baseball.java

--- src/Assignments/Baseball.java
+++ src/Assignments/Baseball.java
@@ -6,18 +6,18 @@ public class Baseball {
     public static void base(Scanner scanner){
         int dodgers;
         int yankees;
-        int orioles;
+        int cubs;
 
         System.out.print("Enter Dodgers number: ");
         dodgers = scanner.nextInt();
         System.out.print("Enter Yankees number: ");
         yankees = scanner.nextInt();
         System.out.print("Enter Orioles number: ");
-        orioles = scanner.nextInt();
+        cubs = scanner.nextInt();
 
         System.out.println("Last night the Dodgers scored "+ dodgers +", ");
         System.out.println("the Yankees scored "+ yankees +", and");
-        System.out.println("the Cubs scored "+ orioles +".");
+        System.out.println("the Cubs scored "+ cubs +".");
 
         scanner.close();
     }
